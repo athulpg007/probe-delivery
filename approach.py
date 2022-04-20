e27b47dc9f76d936573c8391f84bda6959e53a60
Athul P. Girija <athulpg007@gmail.com>
2022-04-20 10:21:43 -0600

    Complete calculation of approach orbital parameters

--- approach.py
+++ approach.py
@@ -1,4 +1,4 @@
-# SOURCE FILENAME : probe_trajectory.py
+# SOURCE FILENAME : approach.py
 # AUTHOR          : Athul Pradeepkumar Girija, apradee@purdue.edu
 # DATE MODIFIED   : 04/19/2022, 19:31 MT
 # REMARKS         : Compute the probe arrival trajectory
@@ -33,11 +33,60 @@ class Approach:
 
         self.phi_2 = np.arctan(self.v_inf_vec_bi_prime[0]/self.v_inf_vec_bi[2])
 
+        self.rp_vec_bi_x_unit = np.cos(self.phi_1)*\
+                                (np.sin(self.beta)*np.cos(self.psi)*np.cos(self.phi_2) + np.cos(self.beta)*np.sin(self.phi_2)) -\
+                                np.sin(self.phi_1)*np.sin(self.beta)*np.sin(self.psi)
+
+        self.rp_vec_bi_y_unit = np.sin(self.phi_1)*\
+                                (np.sin(self.beta)*np.cos(self.psi)*np.cos(self.phi_2) + np.cos(self.beta)*np.sin(self.phi_2)) + \
+                                np.cos(self.phi_1)*np.sin(self.beta)*np.sin(self.psi)
+
+        self.rp_vec_bi_z_unit = np.cos(self.beta)*np.cos(self.phi_2) - np.sin(self.beta)*np.cos(self.psi)*np.sin(self.phi_2)
+
+        self.rp_vec_bi_unit = np.array([self.rp_vec_bi_x_unit,
+                                        self.rp_vec_bi_y_unit,
+                                        self.rp_vec_bi_z_unit])
+
+        self.rp_vec_bi = self.rp*self.rp_vec_bi_unit
+
+        self.rp_vec_bi_dprime = self.rp*np.array([np.sin(self.beta)*np.cos(self.psi),
+                                                  np.sin(self.beta)*np.sin(self.psi),
+                                                  np.cos(self.beta)])
+
+        self.e_vec_bi = self.e*self.rp_vec_bi_unit
+
+        self.e_vec_bi_unit = self.e_vec_bi / LA.linalg.norm(self.e_vec_bi)
+
+        self.h = np.sqrt(self.a*self.planetObj.GM*(1-self.e**2))
+
+        self.h_vec_bi = np.cross(self.rp_vec_bi, self.v_inf_vec_bi)
+
+        self.h_vec_bi_unit = self.h_vec_bi / LA.linalg.norm(self.h_vec_bi)
+
+        self.i = np.arccos(self.h_vec_bi_unit[2])
+
+        self.N_vec_bi_unit = np.cross(np.array([0, 0, 1]), self.h_vec_bi_unit)
+
+        if self.N_vec_bi_unit[1] >= 0:
+            self.OMEGA = np.arccos(self.N_vec_bi_unit[0])
+        else:
+            self.OMEGA = 2*np.pi - np.arccos(self.N_vec_bi_unit[0])
+
+        if self.e_vec_bi_unit[2] >= 0:
+            self.omega = np.arccos(np.dot(self.N_vec_bi_unit, self.e_vec_bi_unit))
+        else:
+            self.omega = 2*np.pi - np.arccos(np.dot(self.N_vec_bi_unit, self.e_vec_bi_unit))
+
 
     def R1(self, theta):
         return np.array([[1, 0, 0],
                         [0, np.cos(theta), np.sin(theta)],
                         [0, -np.sin(theta), np.cos(theta)]])
+
+    def R2(self, theta):
+        return np.array([[np.cos(theta), 0,  -np.sin(theta)],
+                         [0, 1, 0],
+                         [np.sin(theta), 0,  np.cos(theta)]])
 
     def R3(self, theta):
         return np.array([[np.cos(theta), np.sin(theta), 0],
@@ -67,6 +116,10 @@ class Test_Approach_Neptune:
                                                  [0, np.cos(np.pi/2), np.sin(np.pi/2)],
                                                  [0, -np.sin(np.pi/2), np.cos(np.pi/2)]])).all()
 
+    def test_R2_90(self):
+        assert (self.approach.R2(np.pi/2) == np.array([[np.cos(np.pi/2), 0,  -np.sin(np.pi/2)],
+                                                         [0, 1, 0],
+                                                         [np.sin(np.pi/2), 0,  np.cos(np.pi/2)]])).all()
 
     def test_R3_0(self):
 
@@ -121,3 +174,40 @@ class Test_Approach_Neptune:
 
         assert abs(self.approach.phi_2 - phi_2_analytic) < 1e-2
 
+    def test_rp_vec_bi(self):
+
+        ans1 =  (np.matmul(self.approach.R2(self.approach.phi_2),
+                 np.matmul(self.approach.R3(self.approach.phi_1), self.approach.rp_vec_bi.T))).T
+        ans2 = self.approach.rp_vec_bi_dprime
+
+        assert abs(ans1 - ans2).all() < 1e-6
+
+    def test_rp_vec_bi_magnitude(self):
+       assert abs(LA.linalg.norm(self.approach.rp_vec_bi) - self.approach.rp) < 1e-8
+
+    def test_e_vec_bi(self):
+       ans1 = self.approach.e_vec_bi
+       ans2 = np.array([1.15338569, -2.18462991, 0.1522179])
+       assert max(abs(ans1-ans2)) < 1e-6
+
+    def test_h(self):
+        assert abs(self.approach.h - 773198144434.8495)/773198144434.8495 < 1e-8
+
+    def test_h_vec_bi_unit(self):
+        ans1 = self.approach.h_vec_bi_unit
+        ans2 = np.array([-0.15179949, -0.01127846, 0.98834696])
+        assert max(abs(ans1-ans2)) < 1e-6
+
+    def test_i(self):
+        assert abs(self.approach.i*180/np.pi - 8.75547878) < 1e-4
+
+    def test_N_vec_bi_unit(self):
+        ans1 = self.approach.N_vec_bi_unit
+        ans2 = np.array([ 0.01127846, -0.15179949, 0.0])
+        assert max(abs(ans1 - ans2)) < 1e-6
+
+    def test_OMEGA(self):
+        assert abs(self.approach.OMEGA - 4.723667679) < 1e-6
+
+    def test_omega(self):
+        assert abs(self.approach.omega - 1.43110144) < 1e-6
